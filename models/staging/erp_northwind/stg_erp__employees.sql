8a4c77b54d5e2e0f9d34b986a6d881a011c53350
Rafael Moreno <rafael.smoreno@gmail.com>
2023-07-01 13:09:38 +0000

    criada a dim_employees

--- models/staging/erp_northwind/stg_erp__employees.sql
+++ models/staging/erp_northwind/stg_erp__employees.sql
@@ -2,24 +2,18 @@ with
     source_employees as (
         select
             cast(employee_id as int) as funcionario_id,
+            reports_to as gerente_id,
             cast(last_name as string) as sobrenome,
             cast(first_name as string) as nome,
             cast(first_name || ' ' || last_name as string) as nome_completo,
-            -- ,title
-            -- ,title_of_courtesy
-            cast(birth_date as date) as nascimento,
+            cast(birth_date as date) as data_nascimento,
             cast(hire_date as date) as data_contratacao,
             address as endereco,
             city as cidade,
             region as regiao,
             postal_code as cep,
             country as pais,
-            -- ,home_phone 
-            -- ,extension
-            -- ,photo
-            notes as notas,
-            reports_to as gerente_id
-        -- ,photo_path
+            notes as notas
         from {{ source("erp", "employees") }}
     )
 select *
